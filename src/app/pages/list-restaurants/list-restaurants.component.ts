6e038a741810b46584012db2b1d6cbeea8a188e7
Your Name <you@example.com>
2019-12-06 18:47:37 +0100

    23

--- src/app/pages/list-restaurants/list-restaurants.component.ts
+++ src/app/pages/list-restaurants/list-restaurants.component.ts
@@ -7,24 +7,25 @@ import { AlertService } from 'src/app/core/services/alert.service';
   styleUrls: ['./list-restaurants.component.scss']
 })
 export class ListRestaurantsComponent implements OnInit {
-  public items: any = [
-  {id: 1, name: "PIZZA HUT" },
-  {id: 2, name: "ZDROWA KROWA" },
-  {id: 3, name: "PUB OSTRÓWEK" },
-  {id: 4, name: "SHANTI" },
-  {id: 5, name: "TULSI" },
-  {id: 6, name: "KOFEINA 2.0x" },
-  {id: 7, name: "SMAKOBAO" },
-  {id: 8, name: "SAN ESCOBAR" },
-  {id: 9, name: "TYLKO PIZZA" },
-  {id: 10, name: "FRYTKARNIA" },
-  {id: 11, name: "FABUŁA" }];
+	public items: any = [
+		{id: 1, name: "PIZZA HUT" },
+		{id: 2, name: "ZDROWA KROWA" },
+		{id: 3, name: "PUB OSTRÓWEK" },
+		{id: 4, name: "SHANTI" },
+		{id: 5, name: "TULSI" },
+		{id: 6, name: "KOFEINA 2.0x" },
+		{id: 7, name: "SMAKOBAO" },
+		{id: 8, name: "SAN ESCOBAR" },
+		{id: 9, name: "TYLKO PIZZA" },
+		{id: 10, name: "FRYTKARNIA" },
+		{id: 11, name: "FABUŁA" }
+	];
 
   constructor(public alert: AlertService) { }
 
   onDeleteClick(id){
-    this.alert.alertQuestion('Czy napewno chcesz usunąć lokal?');
-    console.log(id); 
+	this.alert.alertQuestion('Czy napewno chcesz usunąć lokal?');
+	console.log(id);
   }
   ngOnInit() {
   }
